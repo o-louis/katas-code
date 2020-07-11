c98011588a870a2a46ed51092e04c0f03c395e5d
Oriane <oriane.louis.pro@gmail.com>
2020-07-11 18:17:40 +0200

    check-the-exam w/ reduce

--- check-the-exam/index.js
+++ check-the-exam/index.js
@@ -9,6 +9,17 @@ function checkExam(array1, array2) {
     return (total < 0 ? 0 : total);
 }
 
+//  w/ REDUCE
+function checkExam(array1, array2) {
+    const total = array1.reduce(function(result, studentAnswer, index){
+        if (studentAnswer === array2[index]) return result += 4;
+        if (array2[index] !== "") return result += -1;
+        return result;
+    }, 0);
+    return (total < 0 ? 0 : total);
+}
+
+
 /* TEST */
 // Test.assertEquals(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6);
 // Test.assertEquals(checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]), 7);
