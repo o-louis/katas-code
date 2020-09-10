369037b271f7bbc6f0498bb1093477d60c0d19ac
Oriane <oriane.louis.pro@gmail.com>
2020-09-10 21:53:27 +0200

    break-camelCase

---
+++ break-camelCase/index.js
@@ -0,0 +1,15 @@
+function solution(string) {
+    let msg = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string[i].charCodeAt(0) >= 65 &&
+            string[i].charCodeAt(0) <= 90) {
+            msg += ' '
+        }
+        msg += string[i];
+    }
+    return msg;
+}
+
+/* TEST */
+// Test.assertEquals(solution('camelCasing'), 'camel Casing', 'Unexpected result')
+// Test.assertEquals(solution('camelCasingTest'), 'camel Casing Test', 'Unexpected result')
